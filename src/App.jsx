b60d894cadca3217aa5bb56d0719529640e24864
asad2002web <asad2002web@gmail.com>
2023-05-18 01:36:01 +0600

    folder structutre and basic routes path completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Button, Navbar } from "flowbite-react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>ToyCraze</div>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+       <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, voluptatibus.</h2>
+    </>
+  )
+}
+
+export default Main
